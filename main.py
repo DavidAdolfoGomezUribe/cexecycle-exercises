0da2ce3f0f121a002075a29bb1852233c9b973c6
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-01 10:21:46 -0500

    test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#Escriba un programa que genere todas las potencias de 2,
-#desde la 0-ésima hasta la ingresada por el usuario:
+#Escriba un programa que pida al usuario dos números enteros, y luego entregue la suma de todos los números que están entre ellos.
+#Por ejemplo, si los números son 1 y 7, debe entregar como resultado 2 + 3 + 4 + 5 + 6 = 20.
 #
-#Ingrese num: 10
-#1 2 4 8 16 32 64 128 256 512 1024
-
+#Ingrese num: 1
+#Ingrese num: 7
+#La suma es 20
+#
 import math
 
 
