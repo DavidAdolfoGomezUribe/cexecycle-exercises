daff233a18ef76533978f9a1f3190ea9effd2e20
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-04 09:34:25 -0500

    Solucion del ejercicio Dibujos de asteriscos

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 #1 2 4 5 8 10 20 25 40 50 100 200
 
 import math
+import sys
+import time
 
 
 
@@ -20,29 +22,86 @@ name = input("    Hello, please enter your full name:  ")
 while True:
 
     print(f""" \n
-        Welcome back Mr/Ms {name}, this is a program for calculate the time in format Hours:Minuts based on the minuts that you put in for your travel: \n""") 
-
+        Welcome back Mr/Ms {name}, this is a program for print 3 types of three types of figures : \n""") 
+        
 
     try :    
-        tTime = 0
-        while True:
-            time = 0
-            time = int(input("        Enter the minutes: "))
-            
-            if time == 0:
-                break
-            elif time > 0:
-                tTime += time
+        select = int(input(f"""        Select the figure that you want :
+        1) for square
+        2) for triangle
+        3) for hexagon \n
+        """))
+        print("")
+        if select > 0 and select < 4:
+           
+            spin_chars = ['|', '/', '-', '\\'," "]
+            for i in range(2):
+                                
+                for char in spin_chars:
+                    
+                    sys.stdout.write(f'\r        {char}')
+                    sys.stdout.flush()
+                    time.sleep(0.1)
+                    
+            if select == 1:    
                 
+                width = int(input("\n        Select the width: "))
+                heigth = int(input("        Select the heigth:  "))
                 
-            else:
-                print("\n        Enter a valid time\n")
-        
-        tHours = tTime // 60
-        tMinuts = tTime % 60
+                if width >= 1 and heigth >= 1 :    
+                    
+                    print("")
+                    matrix = ["*"] * width
 
-        print(f"\n        The time {time} in format HH:MM is {tHours}:{tMinuts} hours for your travel")
-        
+                    for i in range(heigth):
+                        print("        "," ".join(matrix))
+                
+                else:
+                    print("\n        Positive numbers only")
+
+            if select == 2:
+                
+                heigth = int(input("\n        Select the heigth:  "))
+                
+                if  heigth >= 1 :            
+                    matrix = [] 
+                    print("")
+                    for i in range(2 , (heigth + 2 )):
+                        matrix.append("*")
+                        print("        "," ".join(matrix))
+                else:
+                    print("\n        Positive numbers only")
+
+            if select == 3:
+                
+               
+                size = int(input("\n        Select the size: "))
+                print("")
+                
+                if size >= 1  :            
+                    for i in range(size):
+                        
+                        blanck = ' ' * (size - i - 1)
+                        
+                        dots = '*' * (size + 2 * i)
+                        
+                        print("        ",blanck + dots)
+                    
+                    for i in range(size -2,-1,-1):
+                    
+                        blanck = ' ' * (size - i - 1)
+                    
+                        dots = '*' * (size + 2 * i)
+                    
+                        print("        ",blanck + dots)
+                else:
+                    print("\n        Positive numbers only")
+
+
+        else:
+            print ("\n        select the correct program")
+
+
        #  if not (aSide + bSide > cSide and aSide + cSide > bSide and bSide + cSide > aSide):
        #     raise ValueError("") #posiblemente lo use mas tarde
        
